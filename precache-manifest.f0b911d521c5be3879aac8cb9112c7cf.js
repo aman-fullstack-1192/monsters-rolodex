068d487ef5f59a87723f514092df1fc6b0cc02c9
aman kumar <aman.fullstack.1192@gmail.com>
2020-05-03 21:50:17 +0530

    Updates

---
+++ precache-manifest.f0b911d521c5be3879aac8cb9112c7cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "235780fb02bc5e638ec8fc2260fa9a0c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e0e004b280b41f6edf22",
+    "url": "/monsters-rolodex/static/css/main.9fd3721b.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0e004b280b41f6edf22",
+    "url": "/monsters-rolodex/static/js/main.87003b6d.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
